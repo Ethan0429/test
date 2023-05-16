6bed203943c3027860aac03520fdc187f7af5db7
Ethan0429 <ethanrickert0429@gmail.com>
2023-05-16 01:39:03 -0400

    testing

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
 # server.py
 import asyncio
 import websockets
+import os
 
 
 async def echo(websocket, path):
     async for message in websocket:
         print(f"Received message: {message}")
 
-start_server = websockets.serve(echo, "localhost")
+start_server = websockets.serve(
+    echo, "0.0.0.0", os.environ.get("PORT", 8765))
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
